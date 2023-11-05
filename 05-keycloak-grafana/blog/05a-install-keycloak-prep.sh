8fc7e437199d4b09d1f876dde8585edfaa7c21e4
awsandy <awsandy@amazon.co.uk>
2023-11-05 18:44:27 +0000

    up

--- 05-keycloak-grafana/blog/05a-install-keycloak-prep.sh
+++ 05-keycloak-grafana/blog/05a-install-keycloak-prep.sh
@@ -5,6 +5,8 @@ export WORKSPACE_ENDPOINT=$(aws grafana list-workspaces --query 'workspaces[0].e
 echo $WORKSPACE_ENDPOINT
 echo $KEYCLOAK_PASSWORD
 cat << EOF > keycloak_values.yaml
+global:
+  storageClass: gp3
 auth:
   adminUser: admin
   adminPassword: $KEYCLOAK_PASSWORD
