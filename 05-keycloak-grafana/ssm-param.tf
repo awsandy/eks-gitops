c3ac3295ca865beb368ca69aac4b6096517713c0
awsandy <awsandy@amazon.co.uk>
2023-11-06 18:14:02 +0000

    up

--- 05-keycloak-grafana/ssm-param.tf
+++ 05-keycloak-grafana/ssm-param.tf
@@ -1,12 +1,5 @@
-resource "aws_ssm_parameter" "keycloak_cert_arn" {
+data "aws_ssm_parameter" "keycloak_cert_arn" {
   name        = "/workshop/keycloak_cert_arn"
-  description = "keycloak_cert_arn"
-  type        = "String"
-  value       = aws_acm_certificate.keycloak.arn
-
-  tags = {
-    workshop = "EKS Workshop"
-  }
 }
 
 
