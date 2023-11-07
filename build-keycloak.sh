fc2945e2092f340464374b07b5d17c6a65a895fe
awsandy <awsandy@amazon.co.uk>
2023-11-07 22:01:47 +0000

    up

--- build-keycloak.sh
+++ build-keycloak.sh
@@ -4,13 +4,7 @@ time make cluster
 time make addons
 sleep 30
 time make addons
-cd 05-keycloak-grafana/blog
-./05a*
-./05b*
-./06*
-./07*
-cd ../..
-#time make observ
+time make observ
 tend=$(date +%s)
 runtime=$((tend - tstart))
 echo "$com runtime $runtime seconds"
--- build.sh
+++ build.sh
@@ -4,14 +4,13 @@ time make cluster
 time make addons
 sleep 30
 time make addons
-time make grafana
+time make observ
 cd 05-keycloak-grafana/blog
 ./05a*
 ./05b*
 ./06*
 ./07*
 cd ../..
-time make observ
 tend=$(date +%s)
 runtime=$((tend - tstart))
 echo "$com runtime $runtime seconds"
