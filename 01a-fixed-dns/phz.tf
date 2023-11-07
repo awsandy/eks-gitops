3a9efc6de51ade46407a7e5edb18643ccf9bd000
awsandy <awsandy@amazon.co.uk>
2023-11-07 11:13:32 +0000

    up

--- 01a-fixed-dns/phz.tf
+++ 01a-fixed-dns/phz.tf
@@ -1,3 +1,29 @@
 resource "aws_route53_zone" "keycloak" {
   name = format("%s.%s",data.aws_caller_identity.current.account_id,var.dn)
 }
+
+
+
+resource "local_file" "json_config" {
+  content         = <<EOF
+{
+            "Comment": "CREATE NS a record ",
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": ${aws_route53_zone.keycloak.name},
+                                    "Type": "NS",
+                                    "TTL": 300,
+                                 "ResourceRecords": [
+                                  { 
+                                  "Value": ${aws_route53_zone.keycloak.name_servers[0]}},
+                                  "Value": ${aws_route53_zone.keycloak.name_servers[1]}},
+                                  "Value": ${aws_route53_zone.keycloak.name_servers[2]}},
+                                  "Value": ${aws_route53_zone.keycloak.name_servers[4]}}
+                                  ]
+}}]
+}
+EOF
+  filename        = "/home/ec2-user/environment/${data.aws_caller_identity.current.account_id}-dns.json"
+  file_permission = "0640"
+}
