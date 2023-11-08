08144faf1ff073ecde6910fa7c683d815806d1e2
awsandy <awsandy@amazon.co.uk>
2023-11-08 11:49:29 +0000

    up

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,21 @@
 tstart=$(date +%s)
+echo "observ"
 cd 06-observ-accel
 terraform destroy --auto-approve 
+echo "keycloak"
 kubectl delete ns keycloak || true 
 cd ../05-keycloak-grafana
 terraform destroy --auto-approve
+echo "gitops"
 cd ../04-gitops
 terraform destroy --auto-approve
+echo "addons"
 cd ../03-Addons
 terraform destroy --auto-approve
+echo "cluster"
 cd ../02-Cluster
 terraform destroy --auto-approve
+echo "fixed resources"
 cd ../01b-fixed-resources
 terraform destroy --auto-approve
 tend=$(date +%s)
