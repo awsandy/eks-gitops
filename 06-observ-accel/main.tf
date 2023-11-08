d02acf90906706cbaa3bf655ef6cf68b8a14bdfd
awsandy <awsandy@amazon.co.uk>
2023-11-08 19:06:45 +0000

    up

--- 06-observ-accel/main.tf
+++ 06-observ-accel/main.tf
@@ -107,9 +107,12 @@ module "eks_monitoring" {
 
 # Deploy the ADOT Container Insights
 
-#module "eks_container_insights" {
-#  source = "github.com/aws-observability/terraform-aws-observability-accelerator//modules/eks-container-insights"
+#
+module "eks_container_insights" {
+#
+  source = "github.com/aws-observability/terraform-aws-observability-accelerator//modules/eks-container-insights"
 
   #source = "github.com/aws-observability/terraform-aws-observability-accelerator//modules/eks-container-insights?ref=v2.5.4"
-#  eks_cluster_id = data.aws_ssm_parameter.cluster1_name.value
-#}
+#
+  eks_cluster_id = data.aws_ssm_parameter.cluster1_name.value
+}
