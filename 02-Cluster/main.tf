1f98328d4fa3d28c7c98d0966ada4e78d507d595
awsandy <awsandy@amazon.co.uk>
2023-11-08 13:37:00 +0000

    up

--- 02-Cluster/main.tf
+++ 02-Cluster/main.tf
@@ -172,6 +172,7 @@ module "karpenter" {
     AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
   }
 
   tags = local.tags
@@ -258,6 +259,12 @@ resource "kubectl_manifest" "karpenter_node_template" {
             volumeType: gp2
             volumeSize: 16Gi
             deleteOnTermination: true
+      userData: |
+        #!/bin/bash
+        yum install -y amazon-ssm-agent
+        echo "yum'd agent" >> /tmp/me.txt
+        systemctl enable amazon-ssm-agent && systemctl start amazon-ssm-agent
+        echo ec2-user:keycloakpass123 | chpasswd || true
   YAML
 
   depends_on = [
