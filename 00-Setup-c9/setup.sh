f3c460567bc000d459b68ea426ae824b8d778da4
awsandy <awsandy@amazon.co.uk>
2023-11-09 17:29:29 +0000

    up

--- 00-Setup-c9/setup.sh
+++ 00-Setup-c9/setup.sh
@@ -10,15 +10,3 @@ echo "Install eksctl"
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp >/dev/null
 sudo mv -v /tmp/eksctl /usr/local/bin >/dev/null
 eksctl completion bash >>~/.bash_completion
-# auth console:
-c9builder=$(aws cloud9 describe-environment-memberships --environment-id=$C9_PID | jq -r '.memberships[].userArn')
-if echo ${c9builder} | grep -q user; then
-	rolearn=${c9builder}
-        echo Role ARN: ${rolearn}
-elif echo ${c9builder} | grep -q assumed-role; then
-        assumedrolename=$(echo ${c9builder} | awk -F/ '{print $(NF-1)}')
-        rolearn=$(aws iam get-role --role-name ${assumedrolename} --query Role.Arn --output text) 
-        echo Role ARN: ${rolearn}
-fi
-cn=$(aws eks list-clusters --query clusters --output text)
-eksctl create iamidentitymapping --cluster $cn --arn ${rolearn} --group system:masters --username admin
