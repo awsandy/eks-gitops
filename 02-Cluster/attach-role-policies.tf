b2fea7c5a7a12c1822967d2ed0d6b4118fd68b46
awsandy <awsandy@amazon.co.uk>
2023-11-09 10:11:20 +0000

    up

--- 02-Cluster/attach-role-policies.tf
+++ 02-Cluster/attach-role-policies.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role_policy_attachment" "ssmcore" {
-  role       = module.karpenter.aws_iam_instance_profile.this[0].role
+  role       = module.karpenter.aws_iam_instance_profile.role
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 
 resource "aws_iam_role_policy_attachment" "cwserver" {
-  role       = module.karpenter.aws_iam_instance_profile.this[0].role
+  role       = module.karpenter.aws_iam_instance_profile.role
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
