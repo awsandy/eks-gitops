3a35fcbec8538bd8095d5f9af1041225252dbaab
awsandy <awsandy@amazon.co.uk>
2023-11-09 08:52:59 +0000

    up

---
+++ 02-Cluster/inflate.tf
@@ -0,0 +1,39 @@
+# Example deployment using the [pause image](https://www.ianlewis.org/en/almighty-pause-container)
+# and starts with zero replicas
+resource "kubectl_manifest" "karpenter_example_deployment" {
+  yaml_body = <<-YAML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: inflate
+    spec:
+      replicas: 3
+      selector:
+        matchLabels:
+          app: inflate
+      template:
+        metadata:
+          labels:
+            app: inflate
+        spec:
+          terminationGracePeriodSeconds: 0
+          containers:
+            - name: inflate
+              image: public.ecr.aws/eks-distro/kubernetes/pause:3.7
+              resources:
+                requests:
+                  cpu: 1
+          priorityClassName: low-priority
+          affinity:
+            podAntiAffinity:
+              requiredDuringSchedulingIgnoredDuringExecution:
+              - labelSelector:
+                  matchLabels:
+                    app: inflate
+                topologyKey: topology.kubernetes.io/zone
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
