96945e46d600c4e6826f9a1df01f8a1c89701910
awsandy <awsandy@amazon.co.uk>
2023-11-10 17:27:00 +0000

    up

--- build-keycloak.sh
+++ build-keycloak.sh
@@ -2,8 +2,8 @@ tstart=$(date +%s)
 time make fixed
 time make cluster
 time make addons
-sleep 30
-time make addons
+#sleep 30
+#time make addons
 time make observ
 tend=$(date +%s)
 runtime=$((tend - tstart))
