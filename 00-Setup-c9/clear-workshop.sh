3c70dee3c48d4de744990c98a46c06ebe17b0434
awsandy <awsandy@amazon.co.uk>
2023-11-12 11:26:40 +0000

    up

---
+++ 00-Setup-c9/clear-workshop.sh
@@ -0,0 +1,10 @@
+cd ~/environment
+time eksctl delete cluster  --name eks-workshop     # ~8min
+fsid=$(aws efs describe-file-systems --query FileSystems[].FileSystemId --output text)
+for mtid in $(aws efs describe-mount-targets --file-system-id $fsid --query MountTargets[].MountTargetId --output text);do
+echo $mtid
+aws efs delete-mount-target --mount-target-id $mtid
+done
+aws efs delete-file-system --file-system-id $fsid
+dbi=$(aws rds describe-db-instances --query DBInstances[].DBInstanceIdentifier --output text)
+aws rds delete-db-instance --db-instance-identifier $dbi --skip-final-snapshot
