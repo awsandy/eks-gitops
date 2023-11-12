2489d37f71194379ce964163df992ee5fbd82255
awsandy <awsandy@amazon.co.uk>
2023-11-12 18:06:38 +0000

    up

---
+++ 07-observw-keycloak/setup.sh
@@ -0,0 +1,18 @@
+ENV_BASE_DIR=~/environment     # Change this if you executed the `envsetup.sh` script in a different location.
+CLUSTER_NAME=$(aws eks list-clusters | jq -r '.clusters[]')
+WORKSPACE_NAME=keycloak-blog      # Change this if you specified a different name for the AMG workspace.
+KEYCLOAK_NAMESPACE=keycloak
+KEYCLOAK_REALM_AMG=amg
+SETUP_SCRIPT_DIR="${ENV_BASE_DIR}/eks-gitops/07-observw-keycloak"
+if [ ! -f "${SETUP_SCRIPT_DIR}/keycloak-setup.sh" ]; then
+  echo "ERROR: Environment variable ENV_BASE_DIR is set to '${ENV_BASE_DIR}'"
+  echo "ERROR: Script '${SETUP_SCRIPT_DIR}/keycloak-setup.sh' not found."
+  echo 'ERROR: Check if ENV_BASE_DIR is pointing to the location where the envsetup.sh script was executed.'
+else
+  cd $SETUP_SCRIPT_DIR
+  ./keycloak-setup.sh \
+    --cluster-name $CLUSTER_NAME \
+    --workspace-name $WORKSPACE_NAME \
+    --keycloak-namespace $KEYCLOAK_NAMESPACE \
+    --keycloak-realm $KEYCLOAK_REALM_AMG
+fi
