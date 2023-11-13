9706347085b37e956ff942dfac4914f2e9412586
awsandy <awsandy@amazon.co.uk>
2023-11-13 10:13:32 +0000

    up

--- 04-observw-grafana-keycloak/test-setup.sh
+++ 04-observw-grafana-keycloak/test-setup.sh
@@ -6,6 +6,7 @@ kubectl delete statefulset keycloak -n keycloak || true
 kubectl delete statefulset keycloak-postgresql -n keycloak || true
 kubectl delete ns keycloak || true
 aws secretsmanager delete-secret --secret-id oneobservabilityworkshop/keycloak --force-delete-without-recovery || true
+aws iam delete-role --role-name KeycloakSecretStore || true
 ENV_BASE_DIR=~/environment     # Change this if you executed the `envsetup.sh` script in a different location.
 CLUSTER_NAME=$(aws eks list-clusters | jq -r '.clusters[]')
 WORKSPACE_NAME=demo-amg      # Change this if you specified a different name for the AMG workspace.
