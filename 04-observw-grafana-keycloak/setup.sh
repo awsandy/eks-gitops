304f8c7d44ce18c0992f76181b2ec8203d3c36a8
awsandy <awsandy@amazon.co.uk>
2023-11-13 09:49:21 +0000

    up

--- 04-observw-grafana-keycloak/setup.sh
+++ 04-observw-grafana-keycloak/setup.sh
@@ -1,11 +1,11 @@
 acc=$(aws sts get-caller-identity --query Account --output text)
 pa="arn:aws:iam::$acc:policy/OneObservabilityWorkshopKeycloakSecretStorePolicy"
 echo $pa
-aws iam delete-policy --policy-arn $pa
-kubectl delete statefulset keycloak -n keycloak
-kubectl delete statefulset keycloak-postgresql -n keycloak
-kubectl delete ns keycloak
-aws secretsmanager delete-secret --secret-id oneobservabilityworkshop/keycloak --force-delete-without-recovery
+aws iam delete-policy --policy-arn $pa || true
+kubectl delete statefulset keycloak -n keycloak || true
+kubectl delete statefulset keycloak-postgresql -n keycloak || true
+kubectl delete ns keycloak || true
+aws secretsmanager delete-secret --secret-id oneobservabilityworkshop/keycloak --force-delete-without-recovery || true
 ENV_BASE_DIR=~/environment     # Change this if you executed the `envsetup.sh` script in a different location.
 CLUSTER_NAME=$(aws eks list-clusters | jq -r '.clusters[]')
 WORKSPACE_NAME=demo-amg      # Change this if you specified a different name for the AMG workspace.
