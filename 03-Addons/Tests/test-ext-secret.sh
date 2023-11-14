f3e21325c36f11c9e0c27de671aa7ef26deed2c8
awsandy <awsandy@amazon.co.uk>
2023-11-14 14:07:56 +0000

    up

--- 03-Addons/Tests/test-ext-secret.sh
+++ 03-Addons/Tests/test-ext-secret.sh
@@ -2,5 +2,5 @@ echo -n 'KEYID' > ./access-key
 echo -n 'SECRETKEY' > ./secret-access-key
 kubectl create secret generic awssm-secret --from-file=./access-key --from-file=./secret-access-key
 kubectl apply -f basic-secret-store.yaml
-kubectl apply -f "basic-external-secret.yaml
+kubectl apply -f basic-external-secret.yaml
 kubectl describe externalsecret example
