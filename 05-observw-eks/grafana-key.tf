e43e2db2166daaab42c09b4796be650372873888
awsandy <awsandy@amazon.co.uk>
2023-11-14 18:37:46 +0000

    up

--- 05-observw-eks/grafana-key.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_grafana_workspace_api_key" "key" {
-  key_name        = "observability-key"
-  key_role        = "ADMIN"
-  seconds_to_live = 7200
-  workspace_id    = aws_grafana_workspace.workshop.id
-}
-
