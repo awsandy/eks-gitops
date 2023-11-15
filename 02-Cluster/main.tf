9301fcceeb5032e03abbbb3ac8ae057b9b221d19
awsandy <awsandy@amazon.co.uk>
2023-11-15 15:03:14 +0000

    up

--- 02-Cluster/main.tf
+++ 02-Cluster/main.tf
@@ -121,7 +121,7 @@ module "eks" {
   eks_managed_node_groups = {
     mg_5 = {
       node_group_name = "managed-ondemand"
-      instance_types  = ["t3a.xlarge"]
+      instance_types  = ["t3a.large"]
       min_size        = 1
       max_size        = 3
       desired_size    = 2
