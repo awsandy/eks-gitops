db92df54bca673b6f481e9e24029c01ca3d21f4f
awsandy <awsandy@amazon.co.uk>
2023-11-15 15:32:54 +0000

    up

---
+++ 05-observw-eks/destroy.sh
@@ -0,0 +1,14 @@
+cd terraform-aws-observability-accelerator/examples/existing-cluster-with-base-and-infra
+# (mandatory) AWS Region where your resources will be located
+export TF_VAR_aws_region="eu-west-1"
+# (mandatory) EKS Cluster name
+export TF_VAR_eks_cluster_id=$(aws eks list-clusters | jq -r '.clusters[]')
+gid=$(aws grafana list-workspaces --query workspaces[].id --output text)
+export TF_VAR_managed_grafana_workspace_id=$gid
+export TF_VAR_grafana_api_key=`aws grafana create-workspace-api-key --key-name "observability-accelerator-$(date +%s)" --key-role ADMIN --seconds-to-live 1200 --workspace-id $TF_VAR_managed_grafana_workspace_id --query key --output text`
+
+# (optional) Leave it empty for a new workspace to be created
+# managed_prometheus_workspace_id = "prometheus-workspace-id"
+
+#terraform apply -var-file=terraform.tfvars
+terraform destroy -auto-approve
