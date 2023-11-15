4a5fd875c9e61313f8198fdd61314e69173c7327
awsandy <awsandy@amazon.co.uk>
2023-11-15 11:23:48 +0000

    up

--- 02-Cluster/main.tf
+++ 02-Cluster/main.tf
@@ -130,13 +130,21 @@ module "eks" {
       #iam_role_name            = "eks-mng-role"
       #iam_role_use_name_prefix = false
       #iam_role_description     = "EKS managed node group role"
-    iam_role_additional_policies = {
-      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-      AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-      AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"     
-    }
+    #iam_role_additional_policies = {
+    #  AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    #  AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    #  AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    #  AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
+    #  CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"     
+    #}
+    iam_role_additional_policies = [
+       "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+       "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+      "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+      "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
+       "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"     
+    ]
+
   }
 
 
