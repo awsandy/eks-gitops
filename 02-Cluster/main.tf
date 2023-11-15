017e973e7c94ae0c7ec309b12f2ee71b9bfbcae9
awsandy <awsandy@amazon.co.uk>
2023-11-15 11:59:09 +0000

    up

--- 02-Cluster/main.tf
+++ 02-Cluster/main.tf
@@ -54,7 +54,7 @@ locals {
   name            = var.CLUSTER1_NAME
   cluster_version = "1.27"
   region          = var.region
-  iam_role_name="mng-role"
+
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
@@ -121,7 +121,7 @@ module "eks" {
   eks_managed_node_groups = {
     mg_5 = {
       node_group_name = "managed-ondemand"
-      instance_types  = ["t3a.large"]
+      instance_types  = ["t3a.xlarge"]
       min_size        = 1
       max_size        = 3
       desired_size    = 3
@@ -131,13 +131,13 @@ module "eks" {
       #iam_role_name            = "eks-mng-role"
       #iam_role_use_name_prefix = false
       #iam_role_description     = "EKS managed node group role"
-    iam_role_additional_policies = {
-      AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-      AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-      AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-      AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
-      CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"     
-    }
+    #iam_role_additional_policies = {
+    #  AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    #  AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    #  AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    #  AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy",
+    #  CloudWatchAgentServerPolicy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"     
+    #}
     #iam_role_additional_policies = [
     #   "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
     #   "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
