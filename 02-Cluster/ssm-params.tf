2eb2b58614d59e82d4f07467ddd58eab741edf61
awsandy <awsandy@amazon.co.uk>
2023-12-14 10:29:18 +0000

    up

--- 02-Cluster/ssm-params.tf
+++ 02-Cluster/ssm-params.tf
@@ -22,7 +22,7 @@ resource "aws_ssm_parameter" "cluster1_vpc_private_subnets" {
   name        = "/workshop/cluster1_private_subnets"
   description = "The private subnet ids for cluster 1"
   type        = "String"
-  value       = module.vpc.private_subnets[0]
+  value       = module.vpc.private_subnets
   tags = {
     workshop = "EKS Workshop"
   }
@@ -32,7 +32,7 @@ resource "aws_ssm_parameter" "cluster1_vpcid_intra_subnets" {
   name        = "/workshop/cluster1_intra_subnets"
   description = "The intra subnets for cluster 1"
   type        = "String"
-  value       = module.vpc.intra_subnets[0]
+  value       = module.vpc.intra_subnets
   tags = {
     workshop = "EKS Workshop"
   }
