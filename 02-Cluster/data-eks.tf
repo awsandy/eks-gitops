df0be725b28d5309d2bcea3417cf765128ad6c98
awsandy <awsandy@amazon.co.uk>
2023-12-14 16:40:34 +0000

    up

--- 02-Cluster/data-eks.tf
+++ 02-Cluster/data-eks.tf
@@ -1,5 +1,5 @@
 data "aws_iam_openid_connect_provider" "example" {
-  depends_on = [ module.eks]
+  depends_on = [module.eks]
   #url = "https://oidc.eks.eu-west-2.amazonaws.com/id/92689730BC26F44B10C02F4412A09911"
   url=  format("https://%s",module.eks.oidc_provider)
 }
